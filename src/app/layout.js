a973690d35a94b067b4bc8abe00a8cc87f290915
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-01-18 07:09:44 -0500

    last changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,9 @@ import { SideBar } from '@/components/SideBar'
 
 const poppins = Poppins({ weight: ['100','200','300','400','500','600','700','800','900'] ,subsets: ['latin'] })
 
+
+
+
 export const metadata = {
   title: 'Marcelo',
   description: "Marcelo portfolio",
@@ -12,6 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+
+      <link rel="icon" href="/favicon.ico" sizes="16x16" />
+
+
       <body className={poppins.className}>
         <SideBar />
         {children}
